19675de9a22f5be472ae9e943044f530231787b0
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-05 09:52:19 -0600

    morning challenge finished

---
+++ stockPrices.js
@@ -0,0 +1,12 @@
+function best(arr){
+    differences = [];
+    for(let i = 1; i < arr.length; i++){
+        for(let j = 0; j < i; j++){
+            differences.push(arr[i]-arr[j]);
+        }
+    }
+    console.log(differences)
+    return Math.max.apply(null, differences);
+}
+
+console.log(best([15, 10, 20, 22, 1, 9]));
